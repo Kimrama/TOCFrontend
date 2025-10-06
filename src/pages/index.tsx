7c6d815cfaef6eb5aad80618a744351ef570fb59
Kimrama <kimrama0x@gmail.com>
2025-10-06 17:35:05 +0700

    refactor: clean up code formatting and improve readability in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import { useEffect, useState } from 'react';
-import { fetchSongs } from '../api/song';
-import type { Song, SongResponse } from '../interface/song';
-import { Music4, Eye } from 'lucide-react'
-import SearchComponent from '../components/SearchComponent';
-import { FilterComponent } from '../components/FilterComponent';
-import { SongCard } from '../UI/SongCard';
-import { Section } from '../UI/Section';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { fetchSongs } from "../api/song";
+import type { Song, SongResponse } from "../interface/song";
+import { Music4, Eye } from "lucide-react";
+import SearchComponent from "../components/SearchComponent";
+import { FilterComponent } from "../components/FilterComponent";
+import { SongCard } from "../UI/SongCard";
+import { Section } from "../UI/Section";
+import { Link } from "react-router-dom";
 
 function Index() {
   const [popularSongs, setPopularSongs] = useState<Song[]>([]);
@@ -20,11 +20,15 @@ function Index() {
   async function fetchPopularSongs(page: number) {
     setLoading(true);
     try {
-      const response: SongResponse = await fetchSongs({ page, page_size: pageSize, popular: true });
-      setPopularSongs(prevSongs => [...prevSongs, ...response.songs]);
+      const response: SongResponse = await fetchSongs({
+        page,
+        page_size: pageSize,
+        popular: true,
+      });
+      setPopularSongs((prevSongs) => [...prevSongs, ...response.songs]);
       setError(null);
     } catch (err) {
-      setError('Failed to fetch popular songs');
+      setError("Failed to fetch popular songs");
     } finally {
       setLoading(false);
     }
@@ -33,11 +37,15 @@ function Index() {
   async function fetchAllSongs(page: number) {
     setLoading(true);
     try {
-      const response: SongResponse = await fetchSongs({ page, page_size: pageSize, popular: false });
-      setSongs(prevSongs => [...prevSongs, ...response.songs]);
+      const response: SongResponse = await fetchSongs({
+        page,
+        page_size: pageSize,
+        popular: false,
+      });
+      setSongs((prevSongs) => [...prevSongs, ...response.songs]);
       setError(null);
     } catch (err) {
-      setError('Failed to fetch songs');
+      setError("Failed to fetch songs");
     } finally {
       setLoading(false);
     }
@@ -59,7 +67,6 @@ function Index() {
     fetchAllSongs(nextPage);
   };
 
-
   return (
     <div className="flex min-h-screen flex-col ">
       <header className="bg-card border-b border-border/30 shadow-sm">
@@ -71,7 +78,9 @@ function Index() {
               </div>
               <div>
                 <h1 className="text-3xl font-bold text-foreground">LyricDB</h1>
-                <p className="text-muted-foreground">Discover amazing lyric&chord collection with RegEx</p>
+                <p className="text-muted-foreground">
+                  Discover amazing lyric&chord collection with RegEx
+                </p>
               </div>
             </div>
             <div className="border px-4 py-2 rounded-lg hover:bg-accent hover:shadow-md transition cursor-pointer">
@@ -89,9 +98,15 @@ function Index() {
           icon={<Eye />}
           onNextPage={handleNextPopularPage}
         >
-          {
-            popularSongs.map((song, index) => (
-              <Link to={`/song/${encodeURIComponent(song.song)}`} state={{ from: "internal", song: song , image: song.song_transcriber}}>
+          {popularSongs.map((song, index) => (
+            <Link
+              to={`/song/${encodeURIComponent(song.song)}`}
+              state={{
+                from: "internal",
+                song: song,
+                image: song.song_transcriber,
+              }}
+            >
               <SongCard
                 key={index}
                 title={song.song}
@@ -99,9 +114,8 @@ function Index() {
                 views={song.views}
                 image={song.song_transcriber}
               />
-              </Link>
-            ))
-          }
+            </Link>
+          ))}
         </Section>
 
         <Section
@@ -110,9 +124,15 @@ function Index() {
           icon={<Music4 />}
           onNextPage={handleNextSongsPage}
         >
-          {
-            songs.map((song, index) => (
-              <Link to={`/song/${encodeURIComponent(song.song)}`} state={{from: "internal", song: song , image: song.song_transcriber}}>
+          {songs.map((song, index) => (
+            <Link
+              to={`/song/${encodeURIComponent(song.song)}`}
+              state={{
+                from: "internal",
+                song: song,
+                image: song.song_transcriber,
+              }}
+            >
               <SongCard
                 key={index}
                 title={song.song}
@@ -120,9 +140,8 @@ function Index() {
                 views={song.views}
                 image={song.song_transcriber}
               />
-              </Link>
-            ))
-          }
+            </Link>
+          ))}
         </Section>
       </main>
     </div>
